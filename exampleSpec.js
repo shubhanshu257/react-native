3369c851f30138cab99f861996b8cd1c91aafbf2
shubhanshu257 <63340198+shubhanshu257@users.noreply.github.com>
2020-04-20 20:56:03 +0530

    Delete exampleSpec.js

--- exampleSpec.js
+++
@@ -1,9 +0,0 @@
-export default function(spec) {
-  spec.describe('text the input', function() {
-    spec.it('works', async function() {
-
-      await spec.fillIn('Home.TextInput', 'Amy');
-    });
-  });
-
-  
