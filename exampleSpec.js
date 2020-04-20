0aaa3f1418c70bc5d04c70f04de7fc8c35ee0544
shubhanshu257 <63340198+shubhanshu257@users.noreply.github.com>
2020-04-20 21:03:40 +0530

    specs

---
+++ exampleSpec.js
@@ -0,0 +1,9 @@
+export default function(spec) {
+  spec.describe('text the input', function() {
+    spec.it('works', async function() {
+
+      await spec.fillIn('Home.TextInput', 'Shubhanshu');
+    });
+  });
+
+  
