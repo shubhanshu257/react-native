41a903eb210f347987ab7c832c1acb95a832eba4
shubhanshu257 <63340198+shubhanshu257@users.noreply.github.com>
2020-04-21 08:52:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import { Tester, TestHookStore } from 'cavy';
 import App from './Home';
+import ExampleSpec from './specs/exampleSpec';
 
 const testHookStore = new TestHookStore();
 
 class AppWrapper extends Component {
   render() {
     return (
-      <Tester specs={[exampleSpec]} store={testHookStore} >
+      <Tester specs={[ExampleSpec]} store={testHookStore} >
         <App/>
       </Tester>
     );
