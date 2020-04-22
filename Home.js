bd1ce4c4b71e403119fd99b898966c5f8dd58e6a
shubhanshu257 <63340198+shubhanshu257@users.noreply.github.com>
2020-04-22 15:53:07 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react';
-import {View, Text,TextInput,StyleSheet} from 'react-native';
+import {View, Text,TextInput,StyleSheet,Button,Alert} from 'react-native';
 import { hook } from 'cavy';
+
 class Home extends React.Component {
     constructor(){
       super();
-      this.textInput=React.createRef();
+      this.state = {text: ''};  
+      
+     
     }
     render(){
         const {generateTestHook }=this.props;
         return(
             <View>
-          <TextInput
-          ref={generateTestHook('Home.TextInput',this.textInput)}
-            /> 
+            <TextInput 
+            ref ={generateTestHook('Home.TextInput',this.textInput)}
+            onChangeText= {(text) => this.setState({text})}  
+            />
+          <Button
+          ref ={generateTestHook('Home.Button')}
+          title="Press me"
+          onPress={() => Alert.alert(this.state.text)}
+          
+        />
             
             </View>
         );
     }
   }
+
   const TestableScene =hook(Home);
   export default TestableScene;
